a1c137a62ab16576946bdccde508b76b4288dc2f
Your Name <callum0shaw.gmail.com>
2021-02-17 20:40:59 +0200

    FEAT: submit exercise 2.18

--- Part2/phonebook/src/components/AddNameForm.js
+++ Part2/phonebook/src/components/AddNameForm.js
@@ -3,28 +3,41 @@ import services from "/home/callum/Documents/FullStackOpen/Part2/phonebook/src/s
 
 const AddNameForm = (props) => {
   const persons = props.persons;
+  const personExists = persons.find((person) => person.name === props.name);
+  console.log(personExists);
+  const newPerson = { name: props.name, number: props.number };
+
   const handleNumberChange = (event) => props.setNewNumber(event.target.value);
   const handleNameChange = (event) => props.setNewName(event.target.value);
 
-  const alreadyExists = () => {
-    if (persons.some((person) => person.name === props.name)) {
-      return true;
-    } else {
-      return false;
-    }
+  const replaceNumber = () => {
+    services
+      .updateNumber(personExists.id, newPerson)
+      .then(
+        props.setPersons(
+          persons.map((person) =>
+            person.name === props.name ? newPerson : person
+          )
+        )
+      );
+  };
+
+  const createPerson = () => {
+    services.create(newPerson).then((response) => {
+      props.setPersons(persons.concat(newPerson));
+      props.setNewName("");
+      props.setNewNumber("");
+    });
   };
 
   const addName = (event) => {
     event.preventDefault();
-    if (alreadyExists()) {
-      alert(`${props.name} has already been added to phonebook`);
+    if (personExists) {
+      if (window.confirm("Name already exists. Replace number?")) {
+        replaceNumber();
+      }
     } else {
-      const newPerson = { name: props.name, number: props.number };
-      services.create(newPerson).then((response) => {
-        props.setPersons(persons.concat(newPerson));
-        props.setNewName("");
-        props.setNewNumber("");
-      });
+      createPerson();
     }
   };
 
